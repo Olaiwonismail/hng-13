6faae428c4edc1cb30debe32b5dd17702d7d6554
-g <olaiwonismail@gmail.com>
2025-11-02 02:13:31 +0100

    added stage3 task

--- stage-3/app.py
+++ stage-3/app.py
@@ -225,5 +225,5 @@ async def kaith_health_check():
     return {"status": "healthy", "message": "Hehe agent is running"}
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5005))
+    port = int(os.getenv("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
